02e7aa0a9c98a0f9fc5aa436a827bca4e23f558a
cameliachakraborty10-blip <cameliachakraborty10@gmail.com>
2026-09-08 18:06:17 +0530

    Armstrong number program

---
+++ armstrong.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,temp,sum=0,r;
+	cout<<"enter a number";
+	cin>>n;
+	temp=n;
+	while(temp>0)
+	{
+		r=temp%10;
+		sum=sum+r*r*r;
+		temp=temp/10;
+	}
+	if(sum==n)
+	{
+		cout<<"Armstrong number";
+	}
+	else
+	{
+		cout<<"Not armstrong number";
+	}
+	return 0;
+}
