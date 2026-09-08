1b35e4afd5a941d683024189656c851d4436f03d
cameliachakraborty10-blip <cameliachakraborty10@gmail.com>
2026-09-08 18:29:57 +0530

    Call_by_reference program

---
+++ call_by_reference.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void swap(int *x,int *y)
+{
+	
+	int temp;
+	temp=*x;
+	*x=*y;
+	*y=temp;
+	cout<<"\n The values after swapping "<<*x<<" "<<*y;
+}
+
+int main()
+{
+	int a=10;
+	int b=20;
+	cout<<"\n The values before swapping "<<a<<" "<<b;
+	swap(&a,&b);
+	return 0;
+	
+}
